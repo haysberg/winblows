bd2b0fc90b657f547e30795832d62dd284c3971d
Téo le dozo <me@haysberg.io>
2022-01-26 20:13:06 +0100

    Activation à la fin du script

--- setup_windows.ps1
+++ setup_windows.ps1
@@ -21,13 +21,8 @@ choco install thunderbird discord spotify brave steam bitwarden termius 7zip.ins
 Write-Output("Coup de karscher sur le PC...")
 & ([scriptblock]::Create((iwr https://raw.githubusercontent.com/Sycnex/Windows10Debloater/master/Windows10SysPrepDebloater.ps1 -UseBasicParsing))) -Debloat -SysPrep -Privacy
 
-Write-Output("Activation de Windows...")
-iwr -Uri "https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/Separate-Files-Version/Activators/HWID-KMS38_Activation/HWID_Activation.cmd" -OutFile "$HOME\Downloads\activation.cmd"
-& "$HOME\Downloads\activation.cmd" /a
-
 Write-Output("Dark Mode...")
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
-
 
 Write-Output("Tweaks Taskbar")
 #Cacher le volume
@@ -37,3 +32,7 @@ kill -n explorer
 Set-Itemproperty -path 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Feeds' -Name 'ShellFeedsTaskbarViewMode' -value '0'
 
 Set-Itemproperty -path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds' -Name 'ShellFeedsTaskbarViewMode' -value 2
+
+Write-Output("Activation de Windows...")
+iwr -Uri "https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/Separate-Files-Version/Activators/HWID-KMS38_Activation/HWID_Activation.cmd" -OutFile "$HOME\Downloads\activation.cmd"
+& "$HOME\Downloads\activation.cmd" /a
